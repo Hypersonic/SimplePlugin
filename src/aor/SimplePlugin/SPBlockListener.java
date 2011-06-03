8bfa4fb1d4c0f3ea3d5873a372e9f40be762fce4
Josh Hofing <joshhofing@gmail.com>
2011-06-03 17:52:17 -0400

    Blank plugin files

--- src/aor/SimplePlugin/SPBlockListener.java
+++ src/aor/SimplePlugin/SPBlockListener.java
@@ -1,5 +1,21 @@
 package aor.SimplePlugin;
+import org.bukkit.Block;
+import org.bukkit.BlockFace;
+import org.bukkit.Material;
+import org.bukkit.event.block.BlockCanBuildEvent;
+import org.bukkit.event.block.BlockListener;
+import org.bukkit.event.block.BlockPhysicsEvent;
 
-public class SPBlockListener {
+/**
+ * <pluginname> block listener
+ * @author <yourname>
+ */
+public class SPBlockListener extends BlockListener {
+    private final SimplePlugin plugin;
 
+    public SPBlockListener(final SimplePlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    //put all Block related code here
 }
--- src/aor/SimplePlugin/SPPlayerListener.java
+++ src/aor/SimplePlugin/SPPlayerListener.java
@@ -1,5 +1,22 @@
 package aor.SimplePlugin;
 
-public class SPPlayerListener {
+import org.bukkit.Location;
+import org.bukkit.Player;
+import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.PlayerEvent;
+import org.bukkit.event.player.PlayerListener;
+import org.bukkit.event.player.PlayerMoveEvent;
 
+/**
+ * Handle events for all Player related events
+ * @author <yourname>
+ */
+public class SPPlayerListener extends PlayerListener {
+    private final SimplePlugin plugin;
+
+    public SPPlayerListener(SimplePlugin instance) {
+        plugin = instance;
+    }
+
+    //Insert Player related code here
 }
