f315f67f70354e101aaaf51de254191493b29114
herb143 <h.herbie@gmail.com>
2011-06-03 23:48:35 -0400

    Added magnificent lightning and fixed bedrock function.
    Change-Id: I70447e241da312fb67e2ee8ad8e477782288236d

--- src/aor/SimplePlugin/SPPlayerListener.java
+++ src/aor/SimplePlugin/SPPlayerListener.java
@@ -13,6 +13,9 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.block.Block;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.Location;
+import org.bukkit.World;
+
 /* Example Template
  * By Adamki11s
  * HUGE Plugin Tutorial
@@ -29,13 +32,20 @@ public class SPPlayerListener extends PlayerListener {
 		return event.getPlayer().getItemInHand().getType() == Material.GOLD_HOE;
 	}*/
 	
+	
+	
+
 	public void onPlayerInteract(PlayerInteractEvent event) {
-		if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+		if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
 			/// Blah blah blah
 			//Player player = event.getPlayer();
 			//player.sendMessage("You just right clicked something! Congratz!");
-			if (event.getPlayer().getItemInHand().getType() == Material.GOLD_HOE) {
-				(event.getClickedBlock()).setType(Material.BEDROCK); // Set the material to bedrock 'cause they got a GOLD HOE!
+			if (event.getPlayer().getItemInHand().getType() == Material.GOLD_HOE && event.getPlayer().getTargetBlock(null, 200).getType() != Material.AIR) {
+				event.getPlayer().getTargetBlock(null, 200).setType(Material.BEDROCK); // Set the material to bedrock 'cause they got a GOLD HOE!
+		
+				World world = event.getPlayer().getWorld();
+				world.strikeLightningEffect(event.getPlayer().getTargetBlock(null, 200).getLocation());
+				
 			}
 			
 			
