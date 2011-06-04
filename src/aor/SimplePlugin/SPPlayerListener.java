705fc45abf2643a624652290acefdda519d229d6
jayhf <jayhf614@aol.com>
2011-06-04 00:13:13 -0400

    More efficient again

--- src/aor/SimplePlugin/SPPlayerListener.java
+++ src/aor/SimplePlugin/SPPlayerListener.java
@@ -1,15 +1,8 @@
-package aor.SimplePlugin;
-
-
 import java.util.HashSet;
-
-
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.Action;
-import org.bukkit.event.player.PlayerListener;
-import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.*;
 import org.bukkit.block.Block;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
@@ -20,41 +13,28 @@ import org.bukkit.World;
  * By Adamki11s
  * HUGE Plugin Tutorial
  */
-
 public class SPPlayerListener extends PlayerListener {
-	public static SimplePlugin plugin;
-	
-	public SPPlayerListener(SimplePlugin instance) {
-		plugin = instance;
-	}
-	
-	/*public boolean clickedWithHoe(PlayerInteractEvent event) { // The function clickedWithHoe.
-		return event.getPlayer().getItemInHand().getType() == Material.GOLD_HOE;
-	}*/
-	
-	
-	
-
-	public void onPlayerInteract(PlayerInteractEvent event) {
-		if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-			/// Blah blah blah
-			//Player player = event.getPlayer();
-			//player.sendMessage("You just right clicked something! Congratz!");
-			if (event.getPlayer().getItemInHand().getType() == Material.GOLD_HOE && event.getPlayer().getTargetBlock(null, 200).getType() != Material.AIR) {
-				event.getPlayer().getTargetBlock(null, 200).setType(Material.BEDROCK); // Set the material to bedrock 'cause they got a GOLD HOE!
-		
-				World world = event.getPlayer().getWorld();
-				world.strikeLightningEffect(event.getPlayer().getTargetBlock(null, 200).getLocation());
-				
-			}
-			
-			
-		}
-	}
-	
-/*	public void onPlayerMove(PlayerMoveEvent event){
-		
-		Player player = event.getPlayer();
+    public static SimplePlugin plugin;
+    
+    public SPPlayerListener(SimplePlugin instance) {
+        plugin = instance;
+    }
+    /*public boolean clickedWithHoe(PlayerInteractEvent event) { // The function clickedWithHoe.
+        return event.getPlayer().getItemInHand().getType() == Material.GOLD_HOE;
+    }*/
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        /// Blah blah blah
+        //Player player = event.getPlayer();
+        //player.sendMessage("You just right clicked something! Congratz!");
+        if ((event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK)&&event.getPlayer().getItemInHand().getType() == Material.GOLD_HOE && event.getPlayer().getTargetBlock(null, 256).getType() != Material.AIR) {
+			event.getPlayer().getTargetBlock(null, 256).setType(Material.BEDROCK); // Set the material to bedrock 'cause they got a GOLD HOE!
+			event.getPlayer().getWorld().strikeLightning(event.getPlayer().getTargetBlock(null, 256).getLocation());
+        }
+    }
+    
+/*  public void onPlayerMove(PlayerMoveEvent event){
+        
+        Player player = event.getPlayer();
 		Location playerLoc = player.getLocation();
 		
 		player.sendMessage("Your X Coordinates : " + playerLoc.getX());
