5271388078a88bd43d0b706ffcafe3ef5ee703ab
Josh Hofing <joshhofing@gmail.com>
2011-06-04 17:18:28 -0400

    Arraylists and methods, oh my

--- src/aor/SimplePlugin/SpellBook.java
+++ src/aor/SimplePlugin/SpellBook.java
@@ -1,7 +1,16 @@
 package aor.SimplePlugin;
 
 import java.util.ArrayList;
+import aor.SimplePlugin.Spell;
 
 public class SpellBook {
-
+	ArrayList<Spell> spellRegistry = new ArrayList<Spell>();
+	
+	public void registerSpell(Spell sp){
+		spellRegistry.add(sp);
+	}
+	
+	public Spell selectNextSpell(){
+		//TODO: Spell scrolling
+	}
 }
