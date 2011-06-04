99d4375dd6e7cc1fc7ea709396df461fd2b91002
herb143 <h.herbie@gmail.com>
2011-06-03 20:59:54 -0400

    Added a function in SPPLayerListener.java. Just look at it.
    Change-Id: I8a1bf30c18fd5fb1387f70227ef0a7ba74a6d571

--- src/aor/SimplePlugin/SPPlayerListener.java
+++ src/aor/SimplePlugin/SPPlayerListener.java
@@ -2,22 +2,30 @@ package aor.SimplePlugin;
 
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerListener;
 import org.bukkit.event.player.PlayerMoveEvent;
-
+import org.bukkit.event.player.PlayerInteractEvent;
 /* Example Template
  * By Adamki11s
  * HUGE Plugin Tutorial
  */
 
 public class SPPlayerListener extends PlayerListener {
-	
 	public static SimplePlugin plugin;
 	
 	public SPPlayerListener(SimplePlugin instance) {
 		plugin = instance;
 	}
 
+	public void onPlayerInteract(PlayerInteractEvent event) {
+		if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+			/// Blah blah blah
+			Player player = event.getPLayer();
+			player.sendMessage("You just right clicked something! Congratz!");
+		}
+	}
+	
 	public void onPlayerMove(PlayerMoveEvent event){
 		
 		Player player = event.getPlayer();
