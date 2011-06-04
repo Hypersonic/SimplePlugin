85a4a7eb0c625a3440e065f5198fde7a12a485a0
jayhf <jayhf614@aol.com>
2011-06-03 22:51:06 -0400

    More efficient

--- src/aor/SimplePlugin/SPPlayerListener.java
+++ src/aor/SimplePlugin/SPPlayerListener.java
@@ -24,25 +24,17 @@ public class SPPlayerListener extends PlayerListener {
 		plugin = instance;
 	}
 	
-	public boolean clickedWithHoe(PlayerInteractEvent event) { // The function clickedWithHoe.
-		Player player = event.getPlayer();
-		ItemStack itemInHand = player.getItemInHand();
-		if (itemInHand.getType() == Material.GOLD_HOE) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
+	/*public boolean clickedWithHoe(PlayerInteractEvent event) { // The function clickedWithHoe.
+		return event.getPlayer().getItemInHand().getType() == Material.GOLD_HOE;
+	}*/
 	
 	public void onPlayerInteract(PlayerInteractEvent event) {
 		if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
 			/// Blah blah blah
-			Player player = event.getPlayer();
-	//		player.sendMessage("You just right clicked something! Congratz!");
-			Block block = event.getClickedBlock();
-			if (clickedWithHoe(event)) {
-				block.setType(Material.BEDROCK); // Set the material to bedrock 'cause they got a GOLD HOE!
+			//Player player = event.getPlayer();
+			//player.sendMessage("You just right clicked something! Congratz!");
+			if (event.getPlayer().getItemInHand().getType() == Material.GOLD_HOE) {
+				(event.getClickedBlock()).setType(Material.BEDROCK); // Set the material to bedrock 'cause they got a GOLD HOE!
 			}
 			
 			
