6468c791dedd16471926b3cd8e42d02da028aff7
Josh Hofing <joshhofing@gmail.com>
2011-06-03 20:56:33 -0400

    Added Registration for PLAYER_INTERACT to playerListner

--- src/aor/SimplePlugin/SimplePlugin.java
+++ src/aor/SimplePlugin/SimplePlugin.java
@@ -37,7 +37,7 @@ public class SimplePlugin extends JavaPlugin {
         
         pm.registerEvent(Event.Type.PLAYER_MOVE, playerListener, Event.Priority.Normal, this);
         pm.registerEvent(Event.Type.BLOCK_PLACE, blockListener, Event.Priority.Normal, this);
-//	pm.registerEvent(Event.Type.ENTITY_DAMAGE, entityListener, Event.Priority.Normal, this);
+        pm.registerEvent(Event.Type.PLAYER_INTERACT, playerListener, Event.Priority.Normal, this);
 
 /*Some other example listeners
 
