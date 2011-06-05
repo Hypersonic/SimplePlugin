5d0203cadafdcfaed317af2622983ac4cf1e0bea
herb143 <h.herbie@gmail.com>
2011-06-04 20:28:57 -0400

    Added the Bedrock Lightning spell.
    Change-Id: Icca6bae04158b6bebaf1f192d23f8b3a5a3c8742

---
+++ src/aor/SimplePlugin/Spells/BedrockLightningSpell.java
@@ -0,0 +1,33 @@
+package aor.SimplePlugin.Spells;
+
+import org.bukkit.entity.Player;
+import aor.SimplePlugin.Spell;
+import org.bukkit.block.Block;
+import org.bukkit.Material;
+
+
+public class BedrockLightningSpell extends Spell {
+	
+	private static final int MAXDISTANCE = 200; // Sets the maximum distance.
+
+
+	public BedrockLightningSpell() // Constructor.
+	{
+		spellName = "Bedrock Lightning";
+	}
+	
+	
+	public void castSpell(Player player)
+	{
+		Block targetBlock = player.getTargetBlock(null, MAXDISTANCE); // Select the target block.
+		if (targetBlock.getType() != Material.AIR) // No placing bedrock midair!
+		{
+			targetBlock.setType(Material.BEDROCK); // Turn it to bedrock!
+			player.getWorld().strikeLightningEffect(targetBlock.getLocation()); // And strike lightning (fake)!
+			
+		}
+		
+	}
+	
+	
+}
