4c75504de2adc5c6194335ad65b6ff8e714c40dd
herb143 <h.herbie@gmail.com>
2011-06-05 16:17:38 -0400

    Added remove-from-inventory function.
    Based on code by nisovin.
    
    Change-Id: I17186144750c9da73d96840ca1469409d277ed1f

--- src/aor/SimplePlugin/Spell.java
+++ src/aor/SimplePlugin/Spell.java
@@ -6,39 +6,61 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.inventory.Inventory;
 
 public class Spell {
-	
+
 	public Spell() {} // Empty constructor.
-	
+
 	public String spellName;
 	public String getName() { return spellName; }
-	
+
 	public String spellDescription;
 	public String getDescription() { return spellDescription; }
-	
-	
+
+
 	public String returnHello()
 	{
 		return "The spell is working!";
 	}
-	
+
 	public boolean checkRequirements(Player player) { return true; } // Checks if player has the proper requirements for the spell.
-	
+
 
 	public void castSpell(Player player)
 	{
 		player.sendMessage("You're trying to cast a spell that's not set!");
 	}
-	
-	
+
+
 	public void damageItem(Material material, int amount, PlayerInventory inventory) // Damages an item in the inventory. Removes it if it's all used up.
 	{ 
 		ItemStack item = inventory.getItem(inventory.first(material));
 		item.setDurability((short)(item.getDurability() + amount)); // Set durability + amount.
-		
+
 		if (item.getDurability() >= material.getMaxDurability())
 		{ inventory.removeItem(item); } // It's used up.
 	}
-	
+
+	public void removeFromInventory(Inventory inventory, ItemStack item) { // Removes an itemstack from the inventory. Use this for quantities of items. Based on code by nisovin.
+		int amt = item.getAmount();
+		ItemStack[] items = inventory.getContents();
+		for (int i = 0; i < items.length; i++) {
+			if (items[i] != null && items[i].getType() == item.getType() && items[i].getDurability() == item.getDurability()) {
+				if (items[i].getAmount() > amt) {
+					items[i].setAmount(items[i].getAmount() - amt);
+					break;
+				} else if (items[i].getAmount() == amt) {
+					items[i] = null;
+					break;
+				} else {
+					amt -= items[i].getAmount();
+					items[i] = null;
+				}
+			}
+		}
+		inventory.setContents(items);
+	}
+
+
 }
--- src/aor/SimplePlugin/Spells/FlameArrowSpell.java
+++ src/aor/SimplePlugin/Spells/FlameArrowSpell.java
@@ -21,7 +21,7 @@ public class FlameArrowSpell extends Spell {
 		spellDescription = "Shoots an arrow that sets stuff ON FIRE! Requires flint and steel and arrow.";
 	}
 	
-	public boolean checkRequirements(PlayerInventory inventory) // Check inventory function. I understand that this is not optimal but it is in keeping with the superclass.
+	public boolean checkRequirements(PlayerInventory inventory) // TODO: Move this to the superclass entirely.
 	{	
 		if (inventory.contains(Material.FLINT_AND_STEEL) && inventory.contains(Material.ARROW))
 		{ return true; } // They have the proper items.
@@ -35,13 +35,16 @@ public class FlameArrowSpell extends Spell {
 		
 		if (checkRequirements(inventory)) // The reason we don't put it here is because there may be more than just inventory requirements in the future.
 		{
-			damageItem(Material.FLINT_AND_STEEL, 3, inventory);
+			
+			damageItem(Material.FLINT_AND_STEEL, 2, inventory);
+			ItemStack arrowStack = new ItemStack(Material.ARROW, 2);
+			removeFromInventory(inventory,arrowStack);
 			player.sendMessage("This would send a flame arrow, but Herbie didn't code it."); // They have the proper items.
+		
 		}
-		else
-		{
-			player.sendMessage("Could not cast! This spell requires a flint and steel and an arrow!"); // They don't.
-		}
+		
+		
+		else { player.sendMessage("Could not cast! This spell needs flint and steel and an arrow!"); } // They don't have the proper items.
 		
 	}
 	
