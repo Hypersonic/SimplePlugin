1dd2a91d7a329d042b338baad8930cb5e36d927b
herb143 <h.herbie@gmail.com>
2011-06-05 21:38:00 -0400

    Made rapidfire arrow better and rapider-er
    Change-Id: I4e7f80943d05b6f41fa013399ae768d7028003dd

--- src/aor/SimplePlugin/Spells/RapidfireArrowSpell.java
+++ src/aor/SimplePlugin/Spells/RapidfireArrowSpell.java
@@ -15,47 +15,51 @@ import aor.SimplePlugin.Spell;
 
 
 public class RapidfireArrowSpell extends Spell {
-	
-	
+
+
 	public static SimplePlugin plugin;
-	
+
 	private static final int MAXDISTANCE = 200; // Sets the maximum distance.
 
 
 	public RapidfireArrowSpell() // Constructor.
 	{
 		spellName = "Rapidfire Arrow";
-		spellDescription = "Quickly fires off three arrows. Needs three redstone.";
+		spellDescription = "Quickly fires off eight arrows. Needs four redstone.";
 	}
-	
-	
+
+
 	public void castSpell(Player player)
 	{
 		PlayerInventory inventory = player.getInventory();
-		
+
 		// REQUIRED ITEMS
 		ItemStack[] requiredItems = new ItemStack[2]; // The requireditems itemstack.
-		requiredItems[0] = new ItemStack(Material.ARROW, 3); // We need 3 arrows.
-		requiredItems[1] = new ItemStack(Material.REDSTONE, 2); // We need 2 redstone.
+		requiredItems[0] = new ItemStack(Material.ARROW, 8); // We need 8 arrows.
+		requiredItems[1] = new ItemStack(Material.REDSTONE, 4); // We need 2 redstone.
 		// REQUIRED ITEMS
-		
+
 		if (checkInventoryRequirements(inventory, requiredItems))
 		{
 			removeRequiredItemsFromInventory(inventory, requiredItems); // Remove the items.
-			
 			player.shootArrow();
-			player.shootArrow();
-			player.shootArrow();
+			for (int i = 0; i < 7; i++) // Fire off 7 more.
+			{
+				try { Thread.sleep(150); }
+				catch(InterruptedException ae)
+				{ System.out.println(ae); } // If sleep fails
+				
+				player.shootArrow();
+			}
 
-			
 			player.sendMessage("Rapidfire!"); // They have the proper items.
-		
+
 		}
-		
-		
-		else { player.sendMessage("Could not cast! Spell requires 2 redstone, 3 arrow!"); } // They don't have the proper items.
-		
+
+
+		else { player.sendMessage("Could not cast! Spell requires 4 redstone, 8 arrow!"); } // They don't have the proper items.
+
 	}
-	
-	
+
+
 }
