d2bda4453e9512ff8b9122e91729a04bea671dd6
Shanghai <brendanwalsh0@gmail.com>
2011-06-06 14:22:31 -0400

    Got a list of spells made with Michael.

---
+++ snippet/Snippet.java
@@ -0,0 +1,8 @@
+package snippet;
+
+public class Snippet {
+	public static void main(String[] args) {
+		C:\Users\Brendan\SimplePlugin\src\aor\SimplePlugin\Spells\BedrockLightningSpell.java
+	}
+}
+
